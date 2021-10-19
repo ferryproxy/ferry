0d2417e5b51345807c4db7e975c2664483d82b13
Shiming Zhang <wzshiming@foxmail.com>
2021-10-19 13:03:11 +0800

    Fix

--- hack/clusters/up.sh
+++ hack/clusters/up.sh
@@ -11,7 +11,7 @@ ip="$(${dir}/host-docker-internal.sh)"
 echo "Host: ${ip}"
 for name in $(${dir}/kind/list.sh | grep -v control-); do
   kubectl --context="kind-${name}" config view --minify --raw=true > ${out}/"${name}"
-  kubeconfig=$(cat ${out}/"${name}" | sed "s/127.0.0.1/${ip//[[:space:]]/}/g" | grep -v "certificate-authority-data: " | base64)
+  kubeconfig=$(cat ${out}/"${name}" | sed "s/127.0.0.1/${ip//[[:space:]]/}/g" | sed 's/certificate-authority-data: .\+/insecure-skip-tls-verify: true/g' | base64)
   # kubeconfig=$(cat ${out}/"${name}" | base64)
   sed -i "s/:.\+#<host-ip>/: ${ip//[[:space:]]/} #<host-ip>/g" ${dir}/control-plane-cluster/${name}.yaml
   sed -i "s/:.\+#<host-port:${name}>/: $(cat hack/clusters/kind/${name}.yaml | yq '.nodes[0].extraPortMappings[0].hostPort') #<host-port:${name}>/g" ${dir}/control-plane-cluster/${name}.yaml
