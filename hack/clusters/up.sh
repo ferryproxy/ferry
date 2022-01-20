4ae3c63391f6486a8fba751195c326d25ffab428
Shiming Zhang <wzshiming@foxmail.com>
2022-01-20 18:08:56 +0800

    Fix up kind

--- hack/clusters/up.sh
+++ hack/clusters/up.sh
@@ -7,10 +7,9 @@ ${dir}/kind/up.sh
 out=$(realpath "${dir}/../../kubeconfig")
 mkdir -p ${out}
 
-conf=$(realpath "${dir}")
 ip="$(${dir}/kind/host-docker-internal.sh)"
 
-for name in $(${dir}/kind/list.sh | grep -v control-); do
+for name in $(${dir}/kind/list.sh); do
   if [[ ! -f "${out}/${name}.yaml" ]] || [[ "$(cat "${out}/${name}.yaml" | grep "kind-${name}")" == "" ]]; then
       kubeconfig="$(cat "${out}/${name}" | base64)"
       sed -i "s/:.\+#<host-port:${name}>/: 31087 #<host-port:${name}>/g" ${dir}/control-plane-cluster/${name}.yaml
@@ -18,9 +17,9 @@ for name in $(${dir}/kind/list.sh | grep -v control-); do
     continue
   fi
   kubeconfig="$(cat "${out}/${name}" | base64)"
-  sed -i "s/:.\+#<host-ip>/: ${ip//[[:space:]]/} #<host-ip>/g" ${dir}/control-plane-cluster/${name}.yaml
-  sed -i "s/:.\+#<host-port:${name}>/: $(cat hack/clusters/kind/${name}.yaml | yq '.nodes[0].extraPortMappings[0].hostPort') #<host-port:${name}>/g" ${dir}/control-plane-cluster/${name}.yaml
-  sed -i "s/:.\+#<base64-encoded-kubeconfig-data:${name}>/: ${kubeconfig} #<base64-encoded-kubeconfig-data:${name}>/g" ${dir}/control-plane-cluster/${name}.yaml
+  sed -i "s/:.\+#<host-ip>/: ${ip//[[:space:]]/} #<host-ip>/g" "${dir}/control-plane-cluster/${name}.yaml"
+  sed -i "s/:.\+#<host-port:${name}>/: $(cat "hack/clusters/kind/${name}.yaml" | yq '.nodes[0].extraPortMappings[0].hostPort') #<host-port:${name}>/g" "${dir}/control-plane-cluster/${name}.yaml"
+  sed -i "s/:.\+#<base64-encoded-kubeconfig-data:${name}>/: ${kubeconfig} #<base64-encoded-kubeconfig-data:${name}>/g" "${dir}/control-plane-cluster/${name}.yaml"
 done
 
 for name in $(ls ${out} | grep -v .yaml); do
@@ -28,8 +27,10 @@ for name in $(ls ${out} | grep -v .yaml); do
   if [[ ! -f "${out}/${name}.yaml" ]]; then
     kubeconfig="${out}/${name}"
   fi
-  echo kubectl --kubeconfig "${kubeconfig}" apply -k "${conf}/${name}"
-  kubectl --kubeconfig "${kubeconfig}" apply -k "${conf}/${name}"
+  if [[ "${name}" == "control-plane-cluster" ]]; then
+    kubectl --kubeconfig "${kubeconfig}" apply -k https://github.com/ferry-proxy/api/config/crd
+  fi
+  kubectl --kubeconfig "${kubeconfig}" apply -k "${dir}/${name}"
 done
 
 
