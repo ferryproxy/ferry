92876aa7a83f96765cd8f67b634cfef1c2d20c26
kerthcet <kerthcet@gmail.com>
2022-07-29 13:45:40 +0800

    Refactor: signal notify in ferry tunnel
    Signed-off-by: kerthcet <kerthcet@gmail.com>

--- cmd/ferry-tunnel/main.go
+++ cmd/ferry-tunnel/main.go
@@ -19,48 +19,38 @@ package main
 import (
 	"context"
 	"os"
-	"syscall"
+
+	"github.com/go-logr/zapr"
+	"go.uber.org/zap"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 
 	"github.com/ferryproxy/ferry/pkg/consts"
 	"github.com/ferryproxy/ferry/pkg/ferry-tunnel/controller"
 	"github.com/ferryproxy/ferry/pkg/utils/env"
-	"github.com/go-logr/logr"
-	"github.com/go-logr/zapr"
-	"github.com/wzshiming/notify"
-	"go.uber.org/zap"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
+	"github.com/ferryproxy/ferry/pkg/utils/signals"
 )
 
 var (
-	ctx, globalCancel = context.WithCancel(context.Background())
-	log               logr.Logger
-	serviceName       = env.GetEnv("SERVICE_NAME", consts.FerryTunnelName)
-	namespace         = env.GetEnv("NAMESPACE", consts.FerryTunnelNamespace)
-	labelSelector     = env.GetEnv("LABEL_SELECTOR", "tunnel.ferryproxy.io/service=inject")
-	master            = env.GetEnv("MASTER", "")
-	kubeconfig        = env.GetEnv("KUBECONFIG", "")
-	conf              = "./bridge.conf"
+	serviceName   = env.GetEnv("SERVICE_NAME", consts.FerryTunnelName)
+	namespace     = env.GetEnv("NAMESPACE", consts.FerryTunnelNamespace)
+	labelSelector = env.GetEnv("LABEL_SELECTOR", "tunnel.ferryproxy.io/service=inject")
+	master        = env.GetEnv("MASTER", "")
+	kubeconfig    = env.GetEnv("KUBECONFIG", "")
 )
 
-func init() {
+const (
+	conf = "./bridge.conf"
+)
+
+func main() {
 	logConfig := zap.NewDevelopmentConfig()
 	zapLog, err := logConfig.Build()
 	if err != nil {
 		os.Exit(1)
 	}
-	log = zapr.NewLogger(zapLog)
+	log := zapr.NewLogger(zapLog)
 
-	signals := []os.Signal{syscall.SIGINT, syscall.SIGTERM}
-	notify.OnceSlice(signals, func() {
-		globalCancel()
-		notify.OnceSlice(signals, func() {
-			os.Exit(1)
-		})
-	})
-}
-
-func main() {
 	config, err := clientcmd.BuildConfigFromFlags(master, kubeconfig)
 	if err != nil {
 		log.Error(err, "failed to create kubernetes client")
@@ -72,6 +62,15 @@ func main() {
 		log.Error(err, "failed to create kubernetes client")
 		os.Exit(1)
 	}
+
+	stopCh := signals.SetupNotifySignalHandler()
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		<-stopCh
+		cancel()
+	}()
+
 	if serviceName != "" {
 		svcSyncer := controller.NewServiceSyncer(&controller.ServiceSyncerConfig{
 			Clientset:     clientset,
