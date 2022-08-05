e695ca220b9df7e0fe3ac446c6f0271ad330f341
Shiming Zhang <wzshiming@foxmail.com>
2022-08-05 16:03:02 +0800

    Bump version
    Signed-off-by: Shiming Zhang <wzshiming@foxmail.com>

--- pkg/ferryctl/vars/vars.go
+++ pkg/ferryctl/vars/vars.go
@@ -32,7 +32,7 @@ var (
 
 	FerryImagePrefix = env.GetEnv("FERRY_IMAGE_PREFIX", "ghcr.io/ferryproxy/ferry")
 
-	FerryVersion = env.GetEnv("FERRY_VERSION", "v0.4.4")
+	FerryVersion = env.GetEnv("FERRY_VERSION", "v0.5.1")
 
 	FerryControllerImage = env.GetEnv("FERRY_CONTROLLER_IMAGE", FerryImagePrefix+"/ferry-controller:"+FerryVersion)
 
